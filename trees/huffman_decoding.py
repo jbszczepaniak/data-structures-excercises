3043e1c9d4adc9db6ef67f9ceb987ea0e140e3d7
jedrzej <jedrzej@szczepaniak.com>
2017-01-08 18:08:01 +0100

    huffman decoding

--- trees/huffman_decoding.py
+++ trees/huffman_decoding.py
@@ -1,4 +1,21 @@
+import io
 class HuffNode():
     def __init__(self, data, freq):
         self.data = data
         self.freq = freq
+        self.right = None
+        self.left = None
+
+def decode(stream, root_tree):
+    result = []
+    curr_node = root_tree
+
+    for char in stream:
+        if char == '0':
+            curr_node = curr_node.left
+        if char == '1' :
+            curr_node = curr_node.right
+        if not curr_node.left or not curr_node.right:
+            result.append(curr_node.data)
+            curr_node = root_tree
+    return "".join(result)
--- trees/huffman_decoding_test.py
+++ trees/huffman_decoding_test.py
@@ -1,11 +1,27 @@
 import unittest
-from huffman_decoding import HuffNode
+from huffman_decoding import HuffNode, decode
 
-class TestTopView(unittest.TestCase):
-    def test_node_has_frequency_and_data(self):
-        node = HuffNode(data=2, freq=1)
-        self.assertEqual(node.data ,2)
-        self.assertEqual(node.freq ,1)
+class TestHuffmanDecoding(unittest.TestCase):
+    def setUp(self):
+        self.tree = HuffNode(data=0, freq=5)
+        self.tree.left = HuffNode(data=0, freq=2)
+        self.tree.right  = HuffNode(data='A', freq=3)
+        self.tree.left.left = HuffNode(data='B', freq=1)
+        self.tree.left.right = HuffNode(data='C', freq=1)
+
+    def test_one_character_input_stream(self):
+        stream = '1'
+        word = decode(stream, self.tree)
+        self.assertEqual(word, 'A')
+
+    def test_repeating_symbol(self):
+        stream = '111'
+        word = decode(stream, self.tree)
+        self.assertEqual(word, 'AAA')
+
+    def test_hackerrank_input(self):
+        word = decode('1001011', self.tree)
+        self.assertEqual(word,'ABACA')
 
 if __name__ == '__main__':
     unittest.main()
