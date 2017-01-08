c8f7a5afea9b95fe16d21a6056613a4408877e68
jedrzej <jedrzej@szczepaniak.com>
2017-01-08 18:11:29 +0100

    logic mistake

--- trees/huffman_decoding.py
+++ trees/huffman_decoding.py
@@ -15,7 +15,7 @@ def decode(stream, root_tree):
             curr_node = curr_node.left
         if char == '1' :
             curr_node = curr_node.right
-        if not curr_node.left or not curr_node.right:
+        if not curr_node.left and not curr_node.right:
             result.append(curr_node.data)
             curr_node = root_tree
     return "".join(result)
