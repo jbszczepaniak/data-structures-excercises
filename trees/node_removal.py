53b6bf6f70c812d3e11132134f1bccbac24a0090
jedrzej <jedrzej@szczepaniak.com>
2017-03-03 20:14:18 +0100

    Case when node to be removed has one child is done

--- trees/node_removal.py
+++ trees/node_removal.py
@@ -7,5 +7,13 @@ def remove(root, data):
   elif root.data < data:
     root.right = remove(root.right, data)
     return root
-  else:
-    return None
+  else: # This means that
+    if root.left and root.right:
+      return
+      #i don't know
+    elif root.left:
+      return root.left
+    elif root.right:
+      return root.right
+    else:
+      return None
--- trees/node_removal_test.py
+++ trees/node_removal_test.py
@@ -7,6 +7,13 @@ class TestNodeRemoval(unittest.TestCase):
     self.tree = Node(15)
     self.tree.right = Node(20)
     self.tree.left = Node(10)
+
+    self.tree.left.left = Node(8)
+    self.tree.left.left.left = Node(2)
+
+    self.tree.left.right = Node(11)
+    self.tree.left.right.right = Node(12)
+
     self.tree.right.right = Node(30)
     self.tree.right.left = Node(18)
     self.tree.right.right.right = Node(35)
@@ -14,8 +21,16 @@ class TestNodeRemoval(unittest.TestCase):
 
   def test_remove_leaf(self):
     self.tree = remove(self.tree, 25)
-    # print(self.tree.data)
     self.assertTrue(self.tree.right.right.left == None)
+    self.tree = remove(self.tree, 35)
+    self.assertTrue(self.tree.right.right.right == None)
 
+  def test_remove_node_with_left_child(self):
+    self.tree = remove(self.tree, 8)
+    self.assertTrue(self.tree.left.left.data == 2)
+
+  def test_remove_node_with_right_child(self):
+    self.tree = remove(self.tree, 11)
+    self.assertTrue(self.tree.left.right.data == 12)
 if __name__ == '__main__':
   unittest.main()
