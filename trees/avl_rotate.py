eb61fbd76924642330248945cd4f23ad8edb8a7d
jedrzej <jbszczepaniak@gmail.com>
2017-03-04 23:44:37 +0100

    single left and single right rotations works

--- trees/avl_rotate.py
+++ trees/avl_rotate.py
@@ -7,19 +7,17 @@ class AVLNode():
         self.bal_coeff = 0
         self.data = data
         self.height = 0
-        self.right_sub_height = 0
-        self.left_sub_height = 0
         self.right = None
         self.left = None
 
     def update_height(self):
-        self.height = max([self.left_sub_height, self.right_sub_height])
+        self.height = height(self) - 1 # -1 is an adjustment
 
     def update_bal_coeff(self):
-        self.bal_coeff = self.right_sub_height - self.left_sub_height
+        self.bal_coeff = height(self.right) - height(self.left)
 
     def __str__(self):
-        return "data:{}\nheight:{}\nbal_coeff:{}\nleft_sub_tree_h:{}\nright_sub_tree_h:{}\n".format(self.data, self.height, self.bal_coeff, self.left_sub_height, self.right_sub_height)
+        return "data:{}\nheight:{}\nbal_coeff:{}\n".format(self.data, self.height, self.bal_coeff)
 
 def insert_to_avl(data, root):
     if not root:
@@ -28,20 +26,79 @@ def insert_to_avl(data, root):
     elif data < root.data:
         root.left = insert_to_avl(data, root.left)
 
-        root.left_sub_height  = root.left.height + 1 # Only this changes
         root.update_height()
         root.update_bal_coeff()
-        print(root)
+
+        if abs(root.bal_coeff) > 1:
+
+            root = rotate(root)
+
+        # print(root)
 
     elif data >= root.data:
+        # print(data)
         root.right = insert_to_avl(data, root.right)
 
-        root.right_sub_height  = root.right.height  + 1 # Only this changes
         root.update_height()
         root.update_bal_coeff()
-        print(root)
+
+        if abs(root.bal_coeff) > 1:
+            root = rotate(root)
 
     return root
+
+def height(root):
+    if not root:
+        return 0
+    return (max(1 + height(root.left), 1 + height(root.right)))
+
+def rotate(root):
+    if root.bal_coeff > 0:
+        if root.right.bal_coeff >= 0:
+            return rotate_single_left(root)
+        else:
+            return rotate_right_left(root)
+    elif root.bal_coeff < 0:
+        if root.left.bal_coeff < 0:
+            return rotate_single_right(root)
+        else:
+            return rotate_left_right(root)
+
+def rotate_single_left(root):
+    new_root = root.right
+    temp = new_root.left
+    new_root.left = root
+    root.right = temp
+
+    root.update_height()
+    root.update_bal_coeff()
+    new_root.update_height()
+    new_root.update_bal_coeff()
+
+    return new_root
+
+
+def rotate_right_left(root):
+    root.right = rotate_single_right(root.right)
+    return rotate_single_left(root)
+
+def rotate_single_right(root):
+    new_root = root.left
+    temp = new_root.right
+    new_root.right = root
+    root.left = temp
+
+    root.update_height()
+    root.update_bal_coeff()
+    new_root.update_height()
+    new_root.update_bal_coeff()
+
+    return new_root
+
+
+def rotate_left_right(root):
+    root.left = rotate_single_left(root.left)
+    return rotate_single_right(root)
 
 def in_order_traversal(root):
     if root.left:
@@ -52,8 +109,8 @@ def in_order_traversal(root):
 
 if __name__ == '__main__':
     root = AVLNode(10)
-    for num in [5, 12, 15, 16]:
-        insert_to_avl(num, root)
+    for num in [5, 12, 15, 16, 17, 18 , 4, 3 ]:
+        root = insert_to_avl(num, root)
 
-    # add_balance_coeff(root)
-    # in_order_traversal(root)
+    in_order_traversal(root)
+    # print(root.left.left.left)
